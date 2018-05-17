f515a86ca297f4bfb507e4b8edff78ea2e9dcc3f
Mathias Díaz <diaz.mathias.14@gmail.com>
2018-05-17 16:22:02 -0300

    Se agregan llamados a js que se habian quitado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -140,14 +140,19 @@
 
 
 </body>
-	<script src="{{asset('assets/js/jquery.3.2.1.min.js')}}" type="text/javascript"></script>
+    <!--   Core JS Files   -->
+    <script src="{{asset('assets/js/jquery.3.2.1.min.js')}}" type="text/javascript"></script>
     <!--  Charts Plugin -->
-	<script src="{{asset('assets/js/chartist.min.js')}}"></script>
-
+    <script src="{{asset('assets/js/chartist.min.js')}}"></script>
     <!--  Notifications Plugin    -->
     <script src="{{asset('assets/js/bootstrap-notify.js')}}"></script>
+    <!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
+    <script src="{{asset('assets/js/light-bootstrap-dashboard.js?v=1.4.0')}}"></script>
+    <!-- Light Bootstrap Table DEMO methods, don't include it in your project! -->
+    <script src="{{asset('assets/js/demo.js')}}"></script>
 
     <script src="{{asset('assets/js/bootstrap.min.js')}}" type="text/javascript"></script>
-
-	@yield('scripts')
+	
+    @yield('scripts')
+    
 </html>
